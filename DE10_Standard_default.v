2db796761cae0b41843f547c94495050877f6ba5
Michel Kakulphimp <michel@kakulphimp.net>
2020-05-15 23:33:23 -0700

    100% working :)

--- DE10_Standard_default.v
+++ DE10_Standard_default.v
@@ -122,94 +122,23 @@ module DE10_Standard_default(
     //  REG/WIRE declarations
     //=======================================================
 
-    //  For Audio CODEC
-    wire    AUD_CTRL_CLK;    //  For Audio Controller
-
     //  For VGA Controller
     wire    VGA_CTRL_CLK;
-    wire    [9:0] mVGA_R;
-    wire    [9:0] mVGA_G;
-    wire    [9:0] mVGA_B;
-    wire    [19:0] mVGA_ADDR;
     wire    [12:0] TEXT_VRAM_ADDRESS;
     wire    [15:0] TEXT_VRAM_DATA;
 
-    wire    mVGA_CLK;
-    wire    [9:0] mRed;
-    wire    [9:0] mGreen;
-    wire    [9:0] mBlue;
-    wire    VGA_Read;   //  VGA data request
-
-    wire    [9:0] recon_VGA_R;
-    wire    [9:0] recon_VGA_G;
-    wire    [9:0] recon_VGA_B;
-
     wire    DLY_RST;
-    reg     [31:0] Cont;
-    wire    [23:0] mSEG7_DIG;
-
-    wire    mDVAL;
-
-    //audio count
-    reg     [31:0] audio_count;
-    reg     key1_reg;
 
     //=======================================================
     //  Structural coding
     //=======================================================
 
     // initial //  
-                 
-    assign DRAM_DQ          = 16'hzzzz;
-
-    assign AUD_ADCLRCK      = 1'bz;                         
-    assign AUD_DACLRCK      = 1'bz;                         
-    assign AUD_DACDAT       = 1'bz;                         
-    assign AUD_BCLK         = 1'bz;                          
-    assign AUD_XCK          = 1'bz;                          
-                            
-    assign FPGA_I2C_SDAT    = 1'bz;                             
-    assign FPGA_I2C_SCLK    = 1'bz; 
-
-
-    assign GPIO_B           = 36'hzzzzzzzz;
-    assign GPIO_A           = 36'hzzzzzzzz;
-
-    assign AUD_XCK          = AUD_CTRL_CLK;
-    assign AUD_ADCLRCK      =  AUD_DACLRCK;
-
-    //  Enable TV Decoder
-    assign  TD_RESET_N  =   KEY[0];
-
-
-    always@(posedge CLOCK_50 or negedge KEY[0])
-        begin
-            if(!KEY[0])
-                 Cont   <=  0;
-            else
-                 Cont   <=  Cont+1;
-        end
-
-    always@(posedge CLOCK_50)
-        begin
-                 key1_reg   <=  KEY[1];
-            if(key1_reg & (!KEY[1]))
-                audio_count = audio_count + 1;
-        end  
-
-
-    assign LEDR         = KEY[0] ? {Cont[25:24], Cont[25:24], Cont[25:24], Cont[25:24], Cont[25:24]} : 10'h3ff;
-    assign mSEG7_DIG    = KEY[0] ? {Cont[27:24], Cont[27:24], Cont[27:24], Cont[27:24], Cont[27:24], Cont[27:24]} : {6{4'b1000}};
-
-    // 7 segment LUT
-    SEG7_LUT_6 u0  (
-        .oSEG0(HEX0),
-        .oSEG1(HEX1),
-        .oSEG2(HEX2),
-        .oSEG3(HEX3),
-        .oSEG4(HEX4),
-        .oSEG5(HEX5),
-        .iDIG(mSEG7_DIG) );
+    // Generate a 25 MHz pixel clock for VGA
+    reg [15:0] count;
+    reg vga_clock;
+    always @ (posedge CLOCK_50)
+        {vga_clock, count} <= count + 16'h8000;  // divide by 2: (2^16)/2 = 0x8000
 
     // Reset Delay Timer
     Reset_Delay r0  (
@@ -217,25 +146,15 @@ module DE10_Standard_default(
         .oRESET(DLY_RST)
         );
 
-    // Audio and VGA PLL clock
-    VGA_Audio u1(
-        .refclk(CLOCK_50),          // refclk.clk
-        .rst(~DLY_RST),             //  reset.reset
-        .outclk_0(VGA_CTRL_CLK),    // outclk0.clk
-        .outclk_1(AUD_CTRL_CLK),    // outclk1.clk
-        .outclk_2(mVGA_CLK),        // outclk2.clk
-        .locked()                   // locked.export
-        );
-
     text_vram m0(
         .out_text_vmem_data(TEXT_VRAM_DATA),
         .in_text_vmem_address(TEXT_VRAM_ADDRESS),
         );
 
-    assign VGA_CLK = VGA_CTRL_CLK;
+    assign VGA_CLK = vga_clock;
     vga_controller vga_ins(
         .in_reset_n(DLY_RST),
-        .in_vga_clock(VGA_CTRL_CLK),
+        .in_vga_clock(vga_clock),
         .in_text_vmem_data(TEXT_VRAM_DATA),
         .out_blank_n(VGA_BLANK_N),
         .out_h_sync(VGA_HS),
@@ -244,27 +163,6 @@ module DE10_Standard_default(
         .out_g_data(VGA_G),
         .out_r_data(VGA_R),
         .out_text_vmem_address(TEXT_VRAM_ADDRESS),
-        );  
-        
-    AUDIO_DAC u2 (
-        // Audio Side
-        .oAUD_BCK(AUD_BCLK),
-        .oAUD_DATA(AUD_DACDAT),
-        .oAUD_LRCK(AUD_DACLRCK),
-        // Control Signals
-        .iSrc_Select(2'b0),
-        .iCLK_18_4(AUD_CTRL_CLK),
-        .iRST_N( DLY_RST &(!key1_reg))
-        ); 
-                                  
-                                  
-    I2C_AV_Config u3 (
-        // Host Side
-        .iCLK(CLOCK_50),
-        .iRST_N(KEY[0]),
-        // I2C Side
-        .I2C_SCLK(FPGA_I2C_SCLK),
-        .I2C_SDAT(FPGA_I2C_SDAT)
         );
 
 endmodule
