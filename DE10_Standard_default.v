9417420e113acd8a1b0f6b69b6d033d0ba3b153d
Michel Kakulphimp <michel@kakulphimp.net>
2020-05-16 10:33:03 -0700

    Cleaning up project directory

--- DE10_Standard_default.v
+++ DE10_Standard_default.v
@@ -31,6 +31,24 @@ module text_vram(
 
 endmodule
 
+module Reset_Delay(iCLK,oRESET);
+input       iCLK;
+output reg  oRESET;
+reg [19:0]  Cont;
+
+always@(posedge iCLK)
+begin
+    if(Cont!=20'hFFFFF)
+    begin
+        Cont    <=  Cont+1;
+        oRESET  <=  1'b0;
+    end
+    else
+    oRESET  <=  1'b1;
+end
+
+endmodule
+
 module DE10_Standard_default(
 
     //////////// CLOCK //////////
